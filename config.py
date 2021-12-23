533478b15898cda0c7539e1a02b59dc8cd256844
meihuanyuyuyu <1361577068@qq.com>
2021-12-23 08:36:56 +0000

    ultimate

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-k = 5
-batch_size = 2
-lr = 9e-3
-bn = True
-in_c = 3
-num_class = 2
-depth = 5
+k = 5 # k折交叉验证，如果没启用交叉验证，此参数可不管
+batch_size = 2 # 批量大小
+lr = 9e-3 # 学习率大小
+bn = True # 模型是否使用batchnormalize
+in_c = 3 # 模型输入通道数，列如rgb图片为3
+num_class = 2 # 输出类别
+depth = 5 # 模型层数
